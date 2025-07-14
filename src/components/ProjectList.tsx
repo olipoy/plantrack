692ea2508e76b99fe6fdc93187952168a5f373db
olipoy <106843581+olipoy@users.noreply.github.com>
2025-07-14 22:20:40 +0200

    Fix scrolling in project list view

--- src/components/ProjectList.tsx
+++ src/components/ProjectList.tsx
@@ -29,7 +29,7 @@ export const ProjectList: React.FC<ProjectListProps> = ({ projects, onSelectProj
   }
 
   return (
-    <div className="flex-1 overflow-y-auto p-4 space-y-3 pb-4">
+    <div className="flex-1 overflow-y-auto p-4 space-y-3 pb-20">
       {projects.map((project) => (
         <div
           key={project.id}
