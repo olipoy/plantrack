d3955583e42d0cda09fae6167881a84331f01786
olipoy <106843581+olipoy@users.noreply.github.com>
2025-08-12 14:34:31 +0200

    Check current imports in server/db.js

--- server/db.js
+++ server/db.js
@@ -1,7 +1,6 @@
 // Database connection and query utilities
 import pkg from 'pg';
 const { Pool } = pkg;
-import pkg from 'pg';
 import { presign } from './s3.js';
 
 // Create PostgreSQL connection pool
